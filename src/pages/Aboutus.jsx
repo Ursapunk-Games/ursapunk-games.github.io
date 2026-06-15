df5c86e6300265b61931b65bad0342def916bfdd
Synneogbjo <bjorn.haugen@outlook.com>
2026-06-15 09:34:22 +0200

    Replaced placeholder text and images

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -5,7 +5,7 @@ export default function Aboutus(){
     return(
         <>
         <section id="profile-page">
-            <h1>The team</h1>
+            <h1>The Team</h1>
             <article className="profile-card">
                 <img src="./theTeampics/Marthe.jpg" alt="picture of Marthe Ø. Skåra"></img>
                 <h3>Marthe Ø. Skåra</h3>
@@ -29,18 +29,18 @@ export default function Aboutus(){
             <article className="profile-card">
                 <img src="./theTeampics/Sindre.png" alt="Picture of Sindre Arnøy"></img>
                 <h3>Sindre Arnøy</h3>
-                <p>Enviroment Artist</p>
+                <p>Environment Artist</p>
             </article>
             <article className="profile-card">
                 <img src="./theTeampics/Sandra.png" alt="Picture of Sandra T. Kivijervi"></img>
                 <h3>Sandra T. Kivijervi</h3>
-                <p>Composer & SFX</p>
+                <p>Composer</p>
             </article>
         </section>
         <section id="aboutgameStyle">
                 <h3>About Us</h3>
             <article >  
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <p>We’re a small, core team of three founders brought together by a love of cozy, hand-made games. Around us is a talented group of collaborators who help bring Tidewrack's shores, creatures and soundscape to life.</p>
                 <img src="./stickers/Sticker7.png" alt="sticker of seagull"></img>
             </article>
         </section>
